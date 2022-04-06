12f33a658405c3c52e2c988c27241dd78359ce4d
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-04-05 21:23:03 -0600

    Finish project

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #mtf algorithm
+#Universidad del Valle de Guatemala
+#Analisis de Algoritmos
+#Integrantes:
+#Bryann Alfaro
+#Diego de Jesus
+#Julio Herrera
+#Jose Ponce
+#Gabriel Quiroz
 
+#Referencia
+#https://www.programiz.com/python-programming/methods/list/pop
+#https://rosettacode.org/wiki/Move-to-front_algorithm
 from mtf_functions import *
 
 configuracion = [0,1,2,3,4]
@@ -109,6 +120,53 @@ for i in secuencia:
     print('Configuracion inicial', configuracion_after)
     print('Se esta solicitando: ',i)
     configuracion_after, costo = mtf(configuracion_after,i)
+    print('Costo: ', costo)
+    costo_total += costo
+    print('Configuracion con MTF: ',configuracion_after,'\n')
+print('Costo total: ', costo_total,'\n')
+print('Configuracion final: ',configuracion_after,'\n')
+
+print('\n')
+configuracion = [0,1,2,3,4]
+secuencia = []
+secuencia = [4,3,2,1,0,4,3,2,1,0,4,3,2,1,0,4,3,2,1,0] #worst
+secuencia2 = [4,3,3,3,2]
+secuencia3 = [] #best
+for i in range(0,20):
+    secuencia3.append(0)
+configuracion_after = configuracion
+costo_total = 0
+print('\n PARTE F BEST \n')
+index = 0
+for i in secuencia3:
+    costo = 0
+
+    print('Configuracion inicial', configuracion_after)
+    print('Se esta solicitando: ',i)
+    configuracion_after, costo =imtf(configuracion_after,i,secuencia3,index)
+    index+=1
+    print('Costo: ', costo)
+    costo_total += costo
+    print('Configuracion con MTF: ',configuracion_after,'\n')
+print('Costo total: ', costo_total,'\n')
+print('Configuracion final: ',configuracion_after,'\n')
+
+configuracion = [0,1,2,3,4]
+secuencia = []
+secuencia = [4,3,2,1,0,4,3,2,1,0,4,3,2,1,0,4,3,2,1,0] #worst
+
+
+configuracion_after = configuracion
+costo_total = 0
+print('\n PARTE F WORST \n')
+index = 0
+for i in secuencia:
+    costo = 0
+
+    print('Configuracion inicial', configuracion_after)
+    print('Se esta solicitando: ',i)
+    configuracion_after, costo =imtf(configuracion_after,i,secuencia,index)
+    index+=1
     print('Costo: ', costo)
     costo_total += costo
     print('Configuracion con MTF: ',configuracion_after,'\n')
--- mtf_functions.py
+++ mtf_functions.py
@@ -9,3 +9,24 @@ def mtf(config, value):
             config.insert(0,new)
             break
     return config, contador
+
+def imtf(config, value,solicitud,index):
+    contador = 0
+    contador_externo = 0
+    cont1 = 0
+    arr = []
+    for i in config:
+        contador_externo+=1
+        if i == value:
+            contador =contador_externo
+            for i in range(contador-2):
+                arr.append(solicitud[(i+solicitud[index]-1)%len(solicitud)])
+            if len(arr)!=0:
+                for i in arr:
+                    if i == value:
+                        cont1 +=1
+                if cont1 == (contador-2):
+                    new = config.pop(contador_externo-1)
+                    config.insert(0,new)
+                break
+    return config, contador
